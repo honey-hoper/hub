2a8b82e4111c9148c11fa4cc84e30339246e4bf4
Honey <honeyhoper21@gmail.com>
2020-04-19 19:48:49 +0530

    created portfolio section in homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryCardComponent } from './home/category-card/category-card.component';
+import { JobCardComponent } from './home/job-card/job-card.component';
 
 @NgModule({
   declarations: [
   MainComponent,
-  HeaderComponent
+  HeaderComponent,
+  HomeComponent,
+  CategoryCardComponent,
+  JobCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/job-card/job-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.sass']
+})
+export class JobCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
